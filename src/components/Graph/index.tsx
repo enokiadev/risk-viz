317bcca83e64b7707ecd0e2f1402e1e747dfc9dd
enokiadev <hi@enochpro.com>
2023-04-28 16:06:52 +0000

    update: (Graph): show tool tips

--- src/components/Graph/index.tsx
+++ src/components/Graph/index.tsx
@@ -1,23 +1,21 @@
 'use client'
 
 import { FC } from 'react';
-import { LineChart, Line, XAxis } from 'recharts';
-import { useStore } from '@/store';
+import { LineChart, Line, XAxis, Tooltip } from 'recharts';
+import { initialData, useStore } from '@/store';
 
 const Graph: FC = () => {
     const filteredDataByYear = useStore(state => state.filteredDataByYear)
 
     return (
-        <div>
-            <LineChart width={1000} height={200} data={filteredDataByYear}>
-                <Line dataKey="Lat" dot={false} stroke="red" />
-                <Line dataKey="Long" dot={false} stroke="orange" />
-                <Line dataKey="Risk Rating" dot={false} stroke="green" />
-                <Line dataKey="Business Category" dot={false} stroke="blue" />
-                <Line dataKey="Asset Name" dot={false} stroke="blue" />
-                <XAxis dataKey="Year" />
-            </LineChart>
-        </div>
+        <LineChart width={1000} height={200} data={filteredDataByYear}>
+            <Line dataKey="Lat" dot={false} stroke="red" />
+            <Line dataKey="Long" dot={false} stroke="orange" />
+            <Line dataKey="Risk Rating" dot={false} stroke="green" />
+            <Line dataKey="Business Category" dot={false} stroke="blue" />
+            <Line dataKey="Asset Name" dot={false} stroke="blue" /> 
+            <Tooltip />
+        </LineChart>
     )
 }
 
